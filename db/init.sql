734198fca8e19ff0b05634765d9a29701216336f
tamara <tamaranaumovic@gmail.com>
2026-05-25 14:04:59 +0200

    baza fix

--- db/init.sql
+++ db/init.sql
@@ -2,4 +2,4 @@ SET GLOBAL log_error_verbosity = 1;
 ALTER USER 'student'@'%' IDENTIFIED WITH caching_sha2_password BY 'student';
 GRANT ALL PRIVILEGES ON university.* TO 'student'@'%';
 FLUSH PRIVILEGES;
-EXIT;
+
